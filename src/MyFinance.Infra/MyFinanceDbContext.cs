c08707d6b3ef799bc13ee0ab6b4d15e2dc1490f9
Victor Rabelo <victorabelo1697@gmail.com>
2026-01-02 22:09:50 -0300

    feat: using environment variables

--- src/MyFinance.Infra/MyFinanceDbContext.cs
+++ src/MyFinance.Infra/MyFinanceDbContext.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using MyFinance.Domain.Entities;
 
 namespace MyFinance.Infra;
 
-public class MyFinanceDbContext : DbContext
+public class MyFinanceDbContext(IConfiguration configuration) : DbContext
 {
     public DbSet<Transaction> Transaction { get; set; }
     public DbSet<AccountPlan> AccountPlan { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=myfinance;Trusted_Connection=True;");
+        var connectionString = configuration.GetConnectionString("Database");
+        optionsBuilder.UseSqlServer(connectionString);
     }
 }
